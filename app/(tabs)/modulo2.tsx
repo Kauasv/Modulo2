8ff6816e6f8479cb52fd66497fb716f5a521be02
kaua <ksvg77828@gmail.com>
2025-05-21 22:51:34 -0300

    1aula

---
+++ app/(tabs)/modulo2.tsx
@@ -0,0 +1,28 @@
+import { SafeAreaView,ScrollAreaview,Pressable,Text,ActivityIndicator,Modal,View } from "react-native"; 
+import { useState } from "react";
+
+export default function module 2(){
+
+    const App = () => {
+        const [modalVisible, setModalVisible] = useState(false);
+        return; 
+    return (    <SafeAreaView> 
+                    <ScrollAreaview>
+                    <Text>Bem vindo</>
+                    <><Pressable>
+                        <Text>/*link*/
+                        </Text>
+                    </Pressable></>
+                    <Modal> 
+                        <view>
+                            <view>
+                            <Text>
+                            </Text>
+                            </view> 
+                        </view>
+                    </Modal>
+                </ScrollAreaview>
+            
+        <SafeAreaView/>)
+     }
+     }
